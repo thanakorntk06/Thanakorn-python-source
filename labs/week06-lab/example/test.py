e88d948697afffffcfec246ed4a3814857f56360
Thanakorn <Thanakorn.tant@ku.th>
2026-08-14 13:20:12 +0700

    Function

---
+++ labs/week06-lab/example/test.py
@@ -0,0 +1,36 @@
+#part2
+#Example 1 
+Calling greet_person with differnt names:
+Hello, Alice! Nice to meet you.
+Hello Bob! Nice to meet you.
+Hello Charlie! Nice to meet you.
+
+
+#Example 3
+Calculating ractangle areas:
+Rectangle with length 5 and width 3
+Area = 5 * 3 = 10
+
+Rectanhle with lenhth 10 and width 7
+Area = 10 * 7 = 70
+
+
+#part 3 
+#Example 1
+Using functions that return values:
+5 + 3 = 8
+10 + 7 = 17
+Sum of both results: 25
+
+#part 4
+#Example 1 
+Using default parameters:
+Hello, Mr./Ms. Smith!
+Hello, Dr. Johnson!
+Hello, Prof. Brown!
+
+#Example 2
+Mutiple dafault parameters:
+Profile: Alice, Age: 25, Country: Unknown
+Profile: Bob, Age: 25, Country: Unknown
+Profile: Charlie, Age: 30, Country: USA
